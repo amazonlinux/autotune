9c93120d87d63a9697613da379991d9afd2af818
Vallish Vaidyeshwara <vallish@amazon.com>
2019-04-02 20:34:42 +0000

    Create a defined enumeration of high speed networking in EC2 instances
    Signed-off-by: Vallish Vaidyeshwara <vallish@amazon.com>

---
+++ src/ec2sys_autotune/ec2_instance_high_networking_performance.py
@@ -0,0 +1,5 @@
+HIGH_NETWORK_PERFORMANCE = ["100 Gigabit",
+                            "50 Gigabit",
+                            "25 Gigabit",
+                            "20 Gigabit",
+                            "10 Gigabit"]
